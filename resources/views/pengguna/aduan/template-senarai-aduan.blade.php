0e715ae06350e407a01e0d2182cc5ff0d2d76c7c
Amirol Zolkifli <sitehandy@gmail.com>
2024-11-05 09:33:56 +0800

    Update contoh passing data

--- resources/views/pengguna/aduan/template-senarai-aduan.blade.php
+++ resources/views/pengguna/aduan/template-senarai-aduan.blade.php
@@ -4,7 +4,15 @@
 <div class="container-fluid px-4">
     <h1 class="mt-4">Aduan</h1>
     <ol class="breadcrumb mb-4">
-        <li class="breadcrumb-item active">Senarai Aduan</li>
+        <li class="breadcrumb-item active">{{ $pageTitle }}</li>
     </ol>
 </div>
+
+@foreach($senaraiAduan as $aduan)
+
+<li>{{ $aduan['tajuk'] }}</li>
+
+@endforeach
+
+
 @endsection
--- routes/web.php
+++ routes/web.php
@@ -26,7 +26,9 @@ Route::get('/register', function () {
 
 // Routing halaman pengguna
 Route::get('/dashboard', function () {
+
     return view('pengguna.template-dashboard');
+
 });
 
 Route::get('/profile', function () {
@@ -34,7 +36,32 @@ Route::get('/profile', function () {
 });
 
 Route::get('/aduan', function () {
-    return view('pengguna.aduan.template-senarai-aduan');
+
+    $pageTitle  = 'Rekod Aduan';
+
+    // Contoh data senarai aduan
+    $senaraiAduan = [
+        ['id' => 1, 'tajuk' => 'Tajuk 1', 'kandungan' => 'Kandungan 1', 'tarikh' => '2024-11-01', 'status' => 'Selesai'],
+        ['id' => 2, 'tajuk' => 'Tajuk 2', 'kandungan' => 'Kandungan 2', 'tarikh' => '2024-11-02', 'status' => 'Dalam Proses'],
+        ['id' => 3, 'tajuk' => 'Tajuk 3', 'kandungan' => 'Kandungan 3', 'tarikh' => '2024-11-03', 'status' => 'Baru'],
+        ['id' => 4, 'tajuk' => 'Tajuk 4', 'kandungan' => 'Kandungan 4', 'tarikh' => '2024-11-04', 'status' => 'Baru'],
+        ['id' => 5, 'tajuk' => 'Tajuk 5', 'kandungan' => 'Kandungan 5', 'tarikh' => '2024-11-05', 'status' => 'Baru'],
+    ];
+
+    // Cara 1 untuk attach / passing data untuk digunakan di dalam template - method with()
+    // return view('pengguna.aduan.template-senarai-aduan')
+    // ->with('senaraiAduan', $senaraiAduan)
+    // ->with('pageTitle', $pageTitle);
+
+    // Cara 2 untuk attach / passing data untuk digunakan di dalam template - method array()
+    // return view('pengguna.aduan.template-senarai-aduan', [
+    //     'senaraiAduan' => $senaraiAduan,
+    //     'pageTitle' => $pageTitle
+    // ]);
+
+    // Cara 3 untuk attach / passing data untuk digunakan di dalam template - method compact()
+    return view('pengguna.aduan.template-senarai-aduan', compact('senaraiAduan', 'pageTitle'));
+
 });
 
 Route::get('/aduan/baru', function () {
